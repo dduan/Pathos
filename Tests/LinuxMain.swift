6c2817a7730deedbd93457dda7efb8a63fe04595
Daniel Duan <daniel@duan.ca>
2018-06-01 17:25:43 -0700

    Generate path joining tests for Linux

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,6 +4,18 @@
 import XCTest
 @testable import PathosTests
 
+extension JoinPathTests {
+    static var allTests = [
+        ("testSimpleSingleJoining", testSimpleSingleJoining),
+        ("testMultipleJoining", testMultipleJoining),
+        ("testMultipleJoiningWithTrailingSeparators", testMultipleJoiningWithTrailingSeparators),
+        ("testJoiningWithAbsolutePath", testJoiningWithAbsolutePath),
+        ("testPathRepresentableSimpleSingleJoining", testPathRepresentableSimpleSingleJoining),
+        ("testPathRepresentableMultipleJoining", testPathRepresentableMultipleJoining),
+        ("testPathRepresentableMultipleJoiningWithTrailingSeparators", testPathRepresentableMultipleJoiningWithTrailingSeparators),
+        ("testPathRepresentableJoiningWithAbsolutePath", testPathRepresentableJoiningWithAbsolutePath),
+    ]
+}
 extension NormalizePathTests {
     static var allTests = [
         ("testAssertEmptyPathBecomesCurrent", testAssertEmptyPathBecomesCurrent),
@@ -16,5 +28,6 @@ extension NormalizePathTests {
 }
 
 XCTMain([
+    testCase(JoinPathTests.allTests),
     testCase(NormalizePathTests.allTests),
 ])
