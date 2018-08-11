468acb4f6926e22de21b87fef6b160488ddcb4e9
Daniel Duan <daniel@duan.ca>
2018-08-11 15:25:35 -0700

    support Path in FixtureTestCase

--- Tests/PathosTests/FixtureTestCase.swift
+++ Tests/PathosTests/FixtureTestCase.swift
@@ -3,8 +3,13 @@ import XCTest
 
 class FixtureTestCase: XCTestCase {
     let fixtureRoot = normalize(path: "\(#file)/../Fixtures")
+    lazy var fixtureRootPath: Path = { return Path(string: self.fixtureRoot) }()
 
     func fixture(_ pathString: String) -> String {
         return join(path: self.fixtureRoot, withPath: pathString)
+    }
+
+    func fixture(_ path: Path) -> Path {
+        return self.fixtureRootPath.join(with: path)
     }
 }
