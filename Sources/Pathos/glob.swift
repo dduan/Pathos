d7d2ab1281f11ba46facedf12c4dadcc0e3a8174
Daniel Duan <daniel@duan.ca>
2018-10-06 14:38:24 -0700

    Refine glob interface
    Glob is not a valid English word, therefore it's unclear what's the
    right thing to do from API Design Guidelines' point of view.

--- Sources/Pathos/glob.swift
+++ Sources/Pathos/glob.swift
@@ -1,20 +1,20 @@
 // TODO: Missing implementation.
 // TODO: Missing unit tests.
 // TODO: Missing docstring.
-public func glob(withPattern pattern: String) -> [String] {
+public func glob(_ pattern: String) -> [String] {
     fatalError("unimplemented")
 }
 
 extension PathRepresentable {
     // TODO: Missing unit tests.
     // TODO: Missing docstring.
-    public static func glob(withPattern pattern: String) -> [Self] {
-        return Pathos.glob(withPattern:)(pattern).map(Self.init)
+    public static func glob(_ pattern: String) -> [Self] {
+        return Pathos.glob(_:)(pattern).map(Self.init)
     }
 
     // TODO: Missing unit tests.
     // TODO: Missing docstring.
-    public func glob(withPattern pattern: String) -> [Self] {
-        return Self.glob(withPattern: self.join(with: Self(string: pattern)).pathString)
+    public func glob(_ pattern: String) -> [Self] {
+        return Self.glob(_: self.join(with: Self(string: pattern)).pathString)
     }
 }
