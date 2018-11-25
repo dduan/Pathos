b82dd180c8cf69f21f51c54a5f6d65d9c6d9dfde
Daniel Duan <daniel@duan.ca>
2018-11-24 22:52:58 -0800

    Use Xcode 10.1 (#26)
    * Use Xcode 10.1
    
    * update docker

--- Scripts/develop-linux-docker.sh
+++ Scripts/develop-linux-docker.sh
@@ -2,6 +2,6 @@
 
 command -v docker &> /dev/null || { echo >&2 "Install docker https://www.docker.com"; exit 1; }
 
-IMAGE=swift@sha256:642beec6d119c3f2e22740bf91f6ca383727237794db8980d5d00ea71d117394
+IMAGE=swift@sha256:501824b412386aa7ba6d08cd8029b5fdda9b6a1e16d688d910cabdbb371ccb51
 NAME=pathosdev
 docker run -it -v "$PWD":/Pathos --name "$NAME" --rm "$IMAGE"
