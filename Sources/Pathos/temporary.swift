095e5d7d141c6fa5d2ec16cfa5f2f1e6833c8a53
Daniel Duan <daniel@duan.ca>
2018-12-15 16:28:57 -0800

    Remove non-instance-specific methods
    These should stay static since they are not relavent to the specific
    instance.

--- Sources/Pathos/temporary.swift
+++ Sources/Pathos/temporary.swift
@@ -114,26 +114,6 @@ extension PathRepresentable {
 
     // TODO: Missing unit tests.
     // TODO: Missing docstring.
-    public func makeTemporaryFile(withSuffix suffix: String? = nil, prefix: String? = nil, inDirectory directory: String? = nil) -> Self? {
-        do {
-            return Self(string: try Pathos.makeTemporaryFile(withSuffix:prefix:inDirectory:)(suffix, prefix, directory))
-        } catch {
-            return nil
-        }
-    }
-
-    // TODO: Missing unit tests.
-    // TODO: Missing docstring.
-    public func makeTemporaryDirectory(withSuffix suffix: String? = nil, prefix: String? = nil, inDirectory directory: String? = nil) -> Self? {
-        do {
-            return Self(string: try Pathos.makeTemporaryDirectory(withSuffix:prefix:inDirectory:)(suffix, prefix, directory))
-        } catch {
-            return nil
-        }
-    }
-
-    // TODO: Missing unit tests.
-    // TODO: Missing docstring.
     public static func makeTemporaryFile(withSuffix suffix: String? = nil, prefix: String? = nil, inDirectory directory: String? = nil) -> Self? {
         do {
             return Self(string: try Pathos.makeTemporaryFile(withSuffix:prefix:inDirectory:)(suffix, prefix, directory))
