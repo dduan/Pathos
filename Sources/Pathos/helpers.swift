11ee49fa9ae595ce7323d96eae8c3b351d3f5f95
Daniel Duan <daniel@duan.ca>
2018-12-27 17:47:17 -0800

    Fix _stripFromRight (#37)
    The charactor to be stripped was hard-coded. Oops.

--- Sources/Pathos/helpers.swift
+++ Sources/Pathos/helpers.swift
@@ -1,7 +1,7 @@
 func _stripFromRight(_ path: String, _ c: Character) -> String {
     let reversed = path.reversed()
     return reversed
-        .firstIndex { $0 != kSeparatorCharacter }
+        .firstIndex { $0 != c }
         .map { reversed.suffix(from: $0) }
         .map { $0.reversed() }
         .map(String.init)
