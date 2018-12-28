df3991147ff0012afbaa5f4c3496100118d14a2f
Daniel Duan <daniel@duan.ca>
2018-12-27 22:14:51 -0800

    Document glob
    And include a `*` in top-level glob for directories.

--- Sources/Pathos/glob.swift
+++ Sources/Pathos/glob.swift
@@ -25,7 +25,25 @@ private func _glob(_ pattern: String) -> [String] {
         .compactMap { String(validatingUTF8: $0) }
 }
 
-// TODO: Missing docstring.
+/// Find all the path names matching a specified pattern according to the rules used by the Unix shells.
+/// Home directory (tilde, `~`) gets expanded. "Globstar" (`**`) can be included as part of the pattern to
+/// represent all intermediary directories, making the search recursive, similar¹ to pattern expansion on Zsh,
+/// Fish and Bash 4+. This is implemented with libc².
+///
+/// ¹ globstar expansion has many quirks and differences among different implementations. Pathos use the
+/// following algorithm:
+///   - find the leftmost occurance of `**` (ignore the rest of occurances).
+///   - split the pattern by middle of the globstar. So `a/**/c/*.swift`, for example, would become `a/*` and
+///     `*/c/*.swift`.
+///   - perform a non-recursive, system glob with the first half (`a/*` from previous example).
+///   - for each result from previous glob that is a directory, recursively find all of its children.
+///   - perform an `fnmatch` (per POSIX/libc) with the *full* pattern with each child, return the ones that
+///     match.
+///
+/// - Parameter pattern: a pattern according to rules of Unix shells.
+/// - Returns: paths in current working directory that matches the pattern.
+/// - Throws: A `SystemError` if any sub-directory cannot be opened or there's not enough memory to hold all
+///           data for results from listing child paths.
 public func glob(_ pattern: String) throws -> [String] {
     guard let globStarPosition = pattern.firstIndex(of: "**") else {
         return _glob(pattern)
@@ -37,14 +55,30 @@ public func glob(_ pattern: String) throws -> [String] {
             .filter { fnmatch(pattern, $0, 0) == 0 }
     }
 
-    return try _glob(String(pattern[pattern.startIndex ..< globStarPosition]))
+    return try _glob(String(pattern[pattern.startIndex ... globStarPosition]))
         .filter(isDirectory(atPath:))
         .flatMap(recursiveFNMatch)
         .map { _stripFromRight($0, kSeparatorCharacter) }
 }
 
 extension PathRepresentable {
-    // TODO: Missing docstring.
+    /// Find all the path names matching a specified pattern according to the rules used by the Unix shells.
+    /// Home directory (tilde, `~`) gets expanded. "Globstar" (`**`) can be included as part of the pattern to
+    /// represent all intermediary directories, making the search recursive, similar¹ to pattern expansion on Zsh,
+    /// Fish and Bash 4+. This is implemented with libc².
+    ///
+    /// ¹ globstar expansion has many quirks and differences among different implementations. Pathos use the
+    /// following algorithm:
+    ///   - find the leftmost occurance of `**` (ignore the rest of occurances).
+    ///   - split the pattern by middle of the globstar. So `a/**/c/*.swift`, for example, would become `a/*` and
+    ///     `*/c/*.swift`.
+    ///   - perform a non-recursive, system glob with the first half (`a/*` from previous example).
+    ///   - for each result from previous glob that is a directory, recursively find all of its children.
+    ///   - perform an `fnmatch` (per POSIX/libc) with the *full* pattern with each child, return the ones that
+    ///     match.
+    ///
+    /// - Parameter pattern: a pattern according to rules of Unix shells.
+    /// - Returns: paths in current working directory that matches the pattern.
     public static func glob(_ pattern: String) -> [Self] {
         return (try? Pathos.glob(_:)(pattern).map(Self.init)) ?? []
     }
