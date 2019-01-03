f90154c649d9fe7f71f5355830ec872393873bf6
Daniel Duan <daniel@duan.ca>
2019-01-02 19:14:54 -0800

    Implement realPath (#51)
    Implement real path, add docstring, and tests.

---
+++ Tests/PathosTests/RealPathTests.swift
@@ -0,0 +1,56 @@
+import Pathos
+import XCTest
+
+final class RealPathTests: XCTestCase {
+    func testTerminalSymbolIsResolved() throws {
+        XCTAssertEqual(
+            try realPath(ofPath: self.fixture(.goodFileSymbol)),
+            self.fixture(.fileThatExists)
+        )
+    }
+
+    func testIntermediatSymbols() throws {
+        XCTAssertEqual(
+            try realPath(ofPath: self.fixture(.fileInNestedDirectoryViaDirectorySymbol)),
+            self.fixture(.fileInNestedDirectory)
+        )
+    }
+
+    func testPathThatDoesNotExist() throws {
+        XCTAssertEqual(try realPath(ofPath: self.fixture(.noneExistence)), self.fixture(.noneExistence))
+    }
+
+    func testRoot() throws {
+        XCTAssertEqual(try realPath(ofPath: "/"), "/")
+    }
+
+    func testEmpty() throws {
+        XCTAssertEqual(try realPath(ofPath: ""), try getCurrentWorkingDirectory())
+    }
+
+    func testPathRepresentableTerminalSymbolIsResolved() {
+        XCTAssertEqual(
+            self.fixturePath(.goodFileSymbol).realPath.pathString,
+            self.fixture(.fileThatExists)
+        )
+    }
+
+    func testPathRepresentableIntermediatSymbols() {
+        XCTAssertEqual(
+            self.fixturePath(.fileInNestedDirectoryViaDirectorySymbol).realPath.pathString,
+            self.fixture(.fileInNestedDirectory)
+        )
+    }
+
+    func testPathRepresentablePathThatDoesNotExist() {
+        XCTAssertEqual(self.fixturePath(.noneExistence).realPath.pathString, self.fixture(.noneExistence))
+    }
+
+    func testPathRepresentableRoot() {
+        XCTAssertEqual(Path(string: "/").realPath.pathString, "/")
+    }
+
+    func testPathRepresentableEmpty() {
+        XCTAssertEqual(Path(string: "").realPath.pathString, try getCurrentWorkingDirectory())
+    }
+}
