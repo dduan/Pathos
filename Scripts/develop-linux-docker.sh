ae7eed46cb98453f9f6645619a9c01053151eba9
Daniel Duan <daniel@duan.ca>
2019-03-25 22:28:01 -0700

    Support Swift 5

--- Scripts/develop-linux-docker.sh
+++ Scripts/develop-linux-docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 command -v docker &> /dev/null || { echo >&2 "Install docker https://www.docker.com"; exit 1; }
-
-IMAGE=swift@sha256:dd9004042a308eccdf1d6dc960b6ad3b3006c1062eb460d2e62001c35e21f518
+IMAGE=norionomura/swift@sha256:3455f611208d110f04c2420717bf2a5cf1124b23c91690fee1bdc278b8838491
 NAME=pathosdev
 docker run -it -v "$PWD":/Pathos --name "$NAME" --rm "$IMAGE"
