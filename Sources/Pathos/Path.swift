f82a67f4a2a17bb5e5d803a390394028ffb1393a
Daniel Duan <daniel@duan.ca>
2019-03-26 22:45:43 -0700

    Make Path *able
    All the automatic `able`s. ohmy.

--- Sources/Pathos/Path.swift
+++ Sources/Pathos/Path.swift
@@ -1,8 +1,10 @@
 // Default conformation to `PathRepresentable`. See `PathRepresentable` for more.
-public struct Path: PathRepresentable {
+public struct Path: Codable, Equatable, Hashable {
     public let pathString: String
 
     public init(_ string: String) {
         self.pathString = string
     }
 }
+
+extension Path: PathRepresentable {}
