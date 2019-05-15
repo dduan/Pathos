ba1badf65bff8a8f6cd254e6244ff5d512c2eff5
Daniel Duan <daniel@duan.ca>
2019-05-14 21:35:37 -0700

    [CI] Update to Swift 5.0.1

--- Scripts/develop-linux-docker.sh
+++ Scripts/develop-linux-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 command -v docker &> /dev/null || { echo >&2 "Install docker https://www.docker.com"; exit 1; }
-IMAGE=swift@sha256:ccaef3f936bd3cabd184a0caf7c2455eb861182b51e77970be4be72bea116a26
+IMAGE=swift@sha256:6b85beec4ef69d2d520e84c053ae7c745193ee3bbf328f4692dd238dd63f924e
 NAME=pathosdev
 docker run -it -v "$PWD":/Pathos --name "$NAME" --rm "$IMAGE"
