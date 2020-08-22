035b30ca016060257491784b6d7a4431f0661617
Daniel Duan <daniel@duan.ca>
2020-08-22 02:58:05 -0700

    Add OS conditions

--- Sources/Pathos/Constants.swift
+++ Sources/Pathos/Constants.swift
@@ -2,4 +2,8 @@ extension Constants {
     static let currentDirectoryByte: CChar = ".".utf8CString[0]
 }
 
+#if os(Windows)
+public typealias Constants = WindowsConstants
+#else
 public typealias Constants = POSIXConstants
+#endif
