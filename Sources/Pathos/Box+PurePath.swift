3b3718129d458835eef42ec742bb2a7a12cf9d84
Daniel Duan <daniel@duan.ca>
2020-08-22 02:51:21 -0700

    Implement split drive for PureWindowsPath

--- Sources/Pathos/Box+PurePath.swift
+++ Sources/Pathos/Box+PurePath.swift
@@ -1,11 +1,11 @@
 extension Box where Content == Optional<PathParts> {
-    func getOrCreateParts(from bytes: Bytes) -> PathParts {
+    func getOrCreateParts(from bytes: Bytes, isWindows: Bool) -> PathParts {
         let parts: PathParts
 
         if let cachedParts = content {
             parts = cachedParts
         } else {
-            parts = PathParts(bytes: bytes)
+            parts = PathParts(bytes: bytes, isWindows: isWindows)
             content = parts
         }
 
