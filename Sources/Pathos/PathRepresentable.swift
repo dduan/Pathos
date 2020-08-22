50fe9152f44c73a6172b024d9047cb0800613e39
Daniel Duan <daniel@duan.ca>
2020-08-21 22:49:01 -0700

    Clear project for re-implementation

--- Sources/Pathos/PathRepresentable.swift
+++
@@ -1,9 +0,0 @@
-/// A type that represents a path. All Pathos methods is available to the conforming type.
-public protocol PathRepresentable {
-    /// A string value that is the path.
-    var pathString: String { get }
-    /// Initialize the type providing a string as path.
-    ///
-    /// - Parameter string: string value for the path.
-    init(_ string: String)
-}
