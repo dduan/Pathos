977b693ba1d34315716aefba569a21f55fb29607
Daniel Duan <daniel@duan.ca>
2020-08-29 07:49:42 -0700

    Implement Path.workingDirectory for Windows

--- Sources/Pathos/Windows/Path+Windows.swift
+++ Sources/Pathos/Windows/Path+Windows.swift
@@ -1,7 +1,18 @@
 #if os(Windows)
+import WinSDK
 extension Path {
     public static func workingDirectory() throws -> Path {
-        throw SystemError.unspecified(errorCode: 0)
+        let binary = try ContiguousArray<WindowsEncodingUnit>(unsafeUninitializedCapacity: Int(MAX_PATH))
+        { buffer, count in
+            let length = GetCurrentDirectoryW(DWORD(MAX_PATH), buffer.baseAddress)
+            if length == 0 {
+                throw SystemError(errorCode: GetLastError())
+            } else {
+                count = Int(length)
+            }
+        }
+
+        return Path(binary)
     }
 }
 #endif // os(Windows)
