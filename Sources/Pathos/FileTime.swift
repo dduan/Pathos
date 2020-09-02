e4b2721c50222bf0dbb91dcb03ebfe3d1d68df62
Daniel Duan <daniel@duan.ca>
2020-09-01 20:17:33 -0700

    Update FileTime docs

--- Sources/Pathos/FileTime.swift
+++ Sources/Pathos/FileTime.swift
@@ -1,8 +1,9 @@
-/// A time interval broken down into seconds and nanoseconds. This is used to represent a point in time since 1970-01-01 00:00:00 UTC.
+/// A time interval broken down into seconds and nanoseconds. This is used to represent a point in
+/// time since 1970-01-01 00:00:00 UTC
 public struct FileTime {
-    /// The second component of the interval.
+    /// Number of seconds since 1970-01-01 00:00:00 UTC
     public var seconds: Int
 
-    /// The nanosecond component of the interval.
+    /// Number of nanoseconds since `seconds`.
     public var nanoseconds: Int
 }
