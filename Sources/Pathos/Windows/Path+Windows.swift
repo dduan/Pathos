16c36fdc0d83665147a00ab2136c92f9db5049eb
Daniel Duan <daniel@duan.ca>
2020-09-02 00:34:25 -0700

    Implement fetching metadata for Windows

--- Sources/Pathos/Windows/Path+Windows.swift
+++ Sources/Pathos/Windows/Path+Windows.swift
@@ -14,5 +14,45 @@ extension Path {
 
         return Path(binary)
     }
+
+    public func metadata(followSymlink: Bool = false) throws -> Metadata {
+        let binary = try followSymlink ? realPath().binaryString : binaryString
+        var data = WIN32_FIND_DATAW()
+        let handle = FindFirstFileW(binary.cString, &data)
+        if handle == INVALID_HANDLE_VALUE {
+            throw SystemError(code: GetLastError())
+        }
+
+        return Metadata(data)
+    }
+
+    private func realPath() throws -> Path {
+        let handle = CreateFileW(
+            binaryString.cString,
+            0,
+            0,
+            nil,
+            DWORD(OPEN_EXISTING),
+            DWORD(FILE_FLAG_BACKUP_SEMANTICS),
+            nil
+        )
+
+        if handle == INVALID_HANDLE_VALUE {
+            throw SystemError(code: GetLastError())
+        }
+
+        let binary = try ContiguousArray<WindowsEncodingUnit>(unsafeUninitializedCapacity: Int(MAX_PATH))
+        { buffer, count in
+            let length = GetFinalPathNameByHandleW(handle, buffer.baseAddress, DWORD(MAX_PATH), DWORD(FILE_NAME_OPENED))
+
+            if length == 0 {
+                throw SystemError(code: GetLastError())
+            }
+
+            count = Int(length)
+        }
+
+        return Path(binary)
+    }
 }
 #endif // os(Windows)
