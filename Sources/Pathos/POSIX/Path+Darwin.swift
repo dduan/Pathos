01765fd11494f91fd0830c474d4a18f7352b04a9
Daniel Duan <daniel@duan.ca>
2020-09-02 00:26:08 -0700

    Delete dead code

--- Sources/Pathos/POSIX/Path+Darwin.swift
+++ Sources/Pathos/POSIX/Path+Darwin.swift
@@ -1,24 +1,6 @@
 #if canImport(Darwin)
 import Darwin
 
-private func _stat(at path: POSIXBinaryString) throws -> stat {
-    var status = stat()
-    if stat((path + [0]).withUnsafeBufferPointer { $0.baseAddress }, &status) != 0 {
-        throw SystemError(code: errno)
-    }
-
-    return status
-}
-
-private func _lstat(at path: POSIXBinaryString) throws -> stat {
-    var status = stat()
-    if lstat((path + [0]).withUnsafeBufferPointer { $0.baseAddress }, &status) != 0 {
-        throw SystemError(code: errno)
-    }
-
-    return status
-}
-
 extension Path {
     public func metadata(followSymlink: Bool = false) throws -> Metadata {
         var status = stat()
