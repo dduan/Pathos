79c23dacb758b7bbab7263867bf38c8a026377f5
Daniel Duan <daniel@duan.ca>
2020-09-03 20:39:08 -0700

    Implement Path.children() for POSIX

--- Sources/Pathos/POSIX/Path+POSIX.swift
+++ Sources/Pathos/POSIX/Path+POSIX.swift
@@ -14,6 +14,40 @@ extension Path {
 
         throw SystemError.unspecified(errorCode: errno)
     }
+
+    public func children(recursive: Bool = false) throws -> [(Path, FileType)] {
+        var result = [(Path, FileType)]()
+        guard let streamPtr = opendir(binaryString.cString) else {
+            throw SystemError(code: errno)
+        }
+
+        defer {
+            closedir(streamPtr)
+        }
+
+        while let entryPtr = readdir(streamPtr) {
+            let entry = entryPtr.pointee
+            guard let name = withUnsafeBytes(
+                of: entry.d_name,
+                { $0.bindMemory(to: POSIXEncodingUnit.self).baseAddress.map(String.init(cString:))
+                }
+            ),
+                name != ".." && name != "."
+            else {
+                continue
+            }
+
+            let pathType: FileType = POSIXFileType(rawFileType: Int32(entry.d_type))
+            let child = joined(with: name)
+            result.append((child, pathType))
+
+            if recursive && pathType.isDirectory {
+                result += try child.children(recursive: true)
+            }
+        }
+
+        return result
+    }
 }
 
 #endif // !os(Windows)
