05713b9553f07388d879fe19b52a60a77f63e9fe
Daniel Duan <daniel@duan.ca>
2020-09-03 20:39:26 -0700

    Add ls example app

---
+++ Examples/ls/main.swift
@@ -0,0 +1,28 @@
+import Pathos
+
+extension String {
+    func withLeftPad(_ n: Int) -> String {
+        String(repeating: " ", count: max(0, n - count)) + self
+    }
+}
+
+do {
+    for (child, type) in try Path(CommandLine.arguments[1]).children() {
+        let meta = try child.metadata()
+        let permission = (meta.permissions.isReadOnly ? "ReadOnly" : "ReadWrite").withLeftPad("ReadWrite".count)
+        let size = "\(meta.size)".withLeftPad(10)
+        let modified = "\(meta.modified.seconds).\(meta.modified.nanoseconds)".withLeftPad(24)
+        let fileType: String
+        let fileTypeLength = "directory".count
+        if type.isDirectory {
+            fileType = "directory"
+        } else if type.isSymlink {
+            fileType = "symlink".withLeftPad(fileTypeLength)
+        } else {
+            fileType = "file".withLeftPad(fileTypeLength)
+        }
+        print("\(fileType) \(permission) \(size) \(modified) \(child)")
+    }
+} catch {
+    print(error)
+}
