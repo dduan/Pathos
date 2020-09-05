0dab2df3338b614cd80b2f804b2e05231582e4c9
Daniel Duan <daniel@duan.ca>
2020-09-05 14:04:37 -0700

    Store windows file attributes

--- Sources/Pathos/Windows/WindowsAttributes.swift
+++ Sources/Pathos/Windows/WindowsAttributes.swift
@@ -1,6 +1,17 @@
 #if os(Windows)
-struct WindowsAttributes {
-    let isReadOnly: Bool
+
+import WinSDK
+
+public struct WindowsAttributes: OptionSet {
+    public var rawValue: DWORD
+
+    public init(rawValue: DWORD) {
+        self.rawValue = rawValue
+    }
+
+    public var isReadOnly: Bool {
+        rawValue & UInt32(bitPattern: FILE_ATTRIBUTE_READONLY) != 0
+    }
 }
 
 extension WindowsAttributes: Permissions {}
