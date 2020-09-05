de332633fcbe764c62b306bf92d4c0a05fa16e74
Daniel Duan <daniel@duan.ca>
2020-09-05 16:28:59 -0700

    Remove optionality of BinaryString.cString

--- Sources/Pathos/BinaryString.swift
+++ Sources/Pathos/BinaryString.swift
@@ -33,8 +33,8 @@ extension POSIXBinaryString {
         self = ContiguousArray(string.utf8CString.dropLast())
     }
 
-    public var cString: UnsafePointer<CChar>? {
-        (self + [0]).withUnsafeBufferPointer { $0.baseAddress }
+    public var cString: UnsafePointer<CChar> {
+        (self + [0]).withUnsafeBufferPointer { $0.baseAddress! }
     }
 
     public var description: String {
@@ -47,8 +47,8 @@ extension WindowsBinaryString {
         self = ContiguousArray(string.utf16)
     }
 
-    public var cString: UnsafePointer<UInt16>? {
-        (self + [0]).withUnsafeBufferPointer { $0.baseAddress }
+    public var cString: UnsafePointer<UInt16> {
+        (self + [0]).withUnsafeBufferPointer { $0.baseAddress! }
     }
 
     public var description: String {
