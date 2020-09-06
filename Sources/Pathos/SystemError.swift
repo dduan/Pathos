86fe8916215ca78d0c5ef59caf1b92fd2016350c
Daniel Duan <daniel@duan.ca>
2020-09-06 16:43:38 -0700

    Implement Path.makeDirectory (#202)

--- Sources/Pathos/SystemError.swift
+++ Sources/Pathos/SystemError.swift
@@ -1,10 +1,33 @@
+#if os(Windows)
+import WinSDK
+#else
+
+#if canImport(Darwin)
+import Darwin
+#else
+import Glibc
+#endif // canImport(Darwin)
+
+#endif // os(Windows)
+
 /// An error returned by the OS.
-public enum SystemError: Error {
+public enum SystemError: Error, Equatable {
+    case fileExists
     /// Unspecified error returned by the OS.
     case unspecified(errorCode: Code)
 
     public init(code: Code) {
-        self = .unspecified(errorCode: code)
+        switch code {
+        #if !os(Windows)
+        case EEXIST:
+            self = .fileExists
+        #else
+        case UInt32(ERROR_ALREADY_EXISTS):
+            self = .fileExists
+        #endif
+        default:
+            self = .unspecified(errorCode: code)
+        }
     }
 
     #if os(Windows)
