a8de97f64aeb59ae2de55a9dcc28a7243a21570d
Daniel Duan <daniel@duan.ca>
2020-09-20 15:45:11 -0700

    Add a test for making/reading symlink

---
+++ Tests/PathosTests/SymlinkTests.swift
@@ -0,0 +1,14 @@
+import Pathos
+import XCTest
+
+final class SymlinkTests: XCTestCase {
+    func testMakingAndReadingSymlinks() throws {
+        try Path.withTemporaryDirectory { _ in
+            let source = Path("a")
+            try source.write("a")
+            let link = Path("b")
+            try source.makeSymlink(at: link)
+            XCTAssertEqual(try link.readSymlink(), source)
+        }
+    }
+}
