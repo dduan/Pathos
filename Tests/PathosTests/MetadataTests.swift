8c6c298435b27ecfe5c60d11786398a80104855f
Daniel Duan <daniel@duan.ca>
2020-09-28 20:24:13 -0700

    Add more tests for metadata

--- Tests/PathosTests/MetadataTests.swift
+++ Tests/PathosTests/MetadataTests.swift
@@ -2,9 +2,30 @@ import Pathos
 import XCTest
 
 final class MetadataTests: XCTestCase {
-    func testRetrievingMetadata() throws {
-        var metadata: Metadata!
-        XCTAssertNoThrow(metadata = try Path(".").metadata())
-        XCTAssertTrue(metadata.fileType.isDirectory)
+    func testRetrievingDirectoryMetadata() throws {
+        try Path.withTemporaryDirectory { temp in
+            let meta = try temp.metadata()
+            XCTAssertTrue(meta.fileType.isDirectory)
+        }
+    }
+
+    func testRetrievingFileMetadata() throws {
+        try Path.withTemporaryDirectory { _ in
+            let path = Path("a")
+            try path.write("")
+            let meta = try path.metadata()
+            XCTAssertTrue(meta.fileType.isFile)
+        }
+    }
+
+    func testRetrievingSymlinkMetadata() throws {
+        try Path.withTemporaryDirectory { _ in
+            let path = Path("a")
+            try path.write("")
+            let link = Path("b")
+            try path.makeSymlink(at: link)
+            let meta = try link.metadata()
+            XCTAssertTrue(meta.fileType.isSymlink)
+        }
     }
 }
